ce800c8a6c78d37d6e3fe715a8b74d81070d1f23
comedian <machiavell.ian.means@protonmail.com>
2022-12-09 15:51:43 -0500

    not quite day 7 part 2

--- day7/src/lib.rs
+++ day7/src/lib.rs
@@ -19,15 +19,26 @@ impl FileType {
 
 #[derive(Debug)]
 pub enum Command {
-    Cd { dest: String}, 
-    Ls,
+    Cd { dest: String  }, 
+    Ls { res: Vec<String> },
 }
 
 impl From<String> for Command {
     fn from(s: String) -> Self {
-        match s.split(" ").next().unwrap() {
-            "ls" => Command::Ls,
-            "cd" => Command::Cd { dest: s.split(" ").last().unwrap().to_owned()},
+        match &s[..2] {
+            "ls" => Command::Ls { 
+                res: s.split("\n")
+                        .map(|x| x.to_string())
+                        .filter(|y| y != "" && y != "ls")
+                        .collect::<Vec<String>>() 
+            },
+            "cd" => Command::Cd { 
+                dest: s.split(" ")
+                        .last()
+                        .unwrap()
+                        .trim()
+                        .to_string()
+            },
             _ => panic!("Error matching command"),
         }
     }   
@@ -65,40 +76,61 @@ impl FileSystem {
             _ => ()
         }
         // propagate size updates 
-        let mut cd = self.current_dir;
-        loop {
-            match cd { 
-                Some(idx) => {
-                    self.files[idx].size += size;
-                    cd = self.files[idx].parent;
-                },
-                None => break,
-            }
-        }
+        // let mut cd = self.current_dir;
+        // loop {
+        //     match cd { 
+        //         Some(idx) => {
+        //             self.files[idx].size += size;
+        //             cd = self.files[idx].parent;
+        //         },
+        //         None => break,
+        //     }
+        // }
         n
     }
 
-    fn handle_cmd(&mut self, cmd: String) {
-        match Command::from(cmd) {
+    fn handle_cmd(&mut self, cmd: Command) {
+        match cmd {
             Command::Cd { dest } => {
                 match self.files.iter().filter(|x| x.name == dest).next() {
                     None => {
+                        // if changing to directory that doesn't exist yet
+                        // (e.g., '/'), add it 
                         if dest != ".." { 
                             let n = self.add(dest, 0); 
                             self.current_dir = Some(n);
                         }
                         else {
-                            let n = self.current_dir.unwrap();
-                            if n != 0 {
-                                self.current_dir = Some(self.files[n].parent.unwrap())
+                        // move up a directory
+                            match self.current_dir {
+                                Some(n) => { 
+                                    match self.files[n].parent { 
+                                        Some(p) => self.current_dir = Some(p),
+                                        None => (), // do nothing
+                                    }
+                                },
+                                None => panic!("moving up from non-existent dir"), 
                             }
                         } 
                     },
+                    // directory match, change current dir
                     Some(file) => self.current_dir = Some(file.idx),
-                };
+                }
                 println!("changing current dir to: {}", self.files[self.current_dir.unwrap()].name); 
             },
-            _ => println!("adding files to {}", self.files[self.current_dir.unwrap()].name),
+            Command::Ls { res } => {
+                match self.current_dir  {
+                    Some(idx) => {
+                        for i in res.iter() {
+                            _ = match i.split(" ").next().unwrap() {
+                                "dir" => self.add(i.split(" ").last().unwrap().to_owned(), 0),
+                                size => self.add(i.split(" ").last().unwrap().to_owned(), size.parse::<usize>().unwrap()),
+                            };
+                        }
+                    },
+                    None => panic!("ls printed for no current dir"),
+                }       
+            }
         }
     }
 
@@ -112,6 +144,21 @@ impl FileSystem {
                 total += f.size
         }
         total
+    }
+
+    pub fn update_sizes(&mut self) {
+        // propagate file sizes to parents
+        for mut f in self.files
+            .iter()
+            .filter(|y| !y.is_dir()) {
+                match f.parent {
+                    Some(idx) => self.files[idx].size += f.size,
+                    None => panic!("abandoned child!"),
+                }
+        }
+        // propagate directory sizes upward 
+        // for f in self.files
+        //    .iter   
     }
 }
 
@@ -140,14 +187,9 @@ pub fn enumerate(filename: &str) -> FileSystem {
     };
 
     let data = fs::read_to_string(filename).expect("failed to read from file");
-    for mut i in data.lines().map(|x| x.split(" ")) {
-        match i.next().unwrap() {
-            "$" => filesystem.handle_cmd(i.collect::<Vec<&str>>().join(" ")),
-            "dir" => _ = filesystem.add(i.next().unwrap().to_string(), 0),
-            size => {
-                _ = filesystem.add(i.next().unwrap().to_string(), size.parse().unwrap());
-            },
-        };
-    };
+    for mut i in data.trim().split("$ ") {
+        if i == "" { continue } ;
+        filesystem.handle_cmd(Command::from(i.to_owned()));
+    }    
     filesystem
 }
--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,7 +1,8 @@
 use day7::*;
 
 fn main() {
-    let fs = enumerate("./data");
+    let fs = enumerate("./data2");
+    println!("{:#?}", fs);
     println!("Directories with less than {} bytes have total of {} bytes", 
-        100000, fs.filter_less_than(100000));
+         100000, fs.filter_less_than(100000));
 }
